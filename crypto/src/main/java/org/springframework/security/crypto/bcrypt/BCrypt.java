a8dc783634805cf13cd5a7cd17c7fa15243a48eb
Sébastien Gondron <s.gondron@sig.eu>
2022-11-23 22:28:58 +0100

    a complex method

--- crypto/src/main/java/org/springframework/security/crypto/bcrypt/BCrypt.java
+++ crypto/src/main/java/org/springframework/security/crypto/bcrypt/BCrypt.java
@@ -254,6 +254,54 @@ public class BCrypt {
 		}
 	}
 
+  public String aComplexMethod() {
+		int a = 3;
+        switch (a) {
+			case 1: return "a";
+			case 2: return "b";
+			case 3: return "c";
+			case 4: return "d";
+			case 5: return "e";
+			case 6: return "f";
+			case 7: return "g";
+			case 8: return "h";
+      case 9: return "i";
+      case 10: return "j";
+      case 11: return "k";
+      case 12: return "l":
+      case 13: return "m";
+      case 14: return "n";
+      case 15: return "o";
+      case 16: return "p";
+      case 17: return "q";
+      case 18: return "r";
+      case 19: return "s";
+      case 20: return "t";
+      case 21: return "u";
+      case 22: return "v";
+      case 23: return "w";
+      case 24: return "x";
+      case 25: return "y";
+			case 26: return "aa";
+			case 27: return "ab";
+			case 28: return "ac";
+			case 29: return "ad";
+			case 30: return "ae";
+			case 31: return "af";
+			case 32: return "ag";
+			case 33: return "ah";
+      case 34: return "ai";
+      case 35: return "aj";
+      case 36: return "ak";
+      case 37: return "al":
+      case 38: return "am";
+      case 39: return "an";
+      case 40: return "ao";
+			default: return "z";
+		}
+	}
+
+
 	/**
 	 * Look up the 3 bits base64-encoded by the specified character, range-checking againt
 	 * conversion table
